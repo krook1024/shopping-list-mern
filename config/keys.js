eaec22b7bded164f7290b2b4fa0f86941ec1045f
Molnár Antal Albert <krook1024@gmail.com>
2020-07-10 21:48:53 +0200

    Implement the backend using Express

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://app:1q2w3e4r@cluster0.pylil.mongodb.net/shopping_list?retryWrites=true&w=majority'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route   GET /api/items
+// @desc    Get all items.
+// @access  Public
+router.get('/', async (req, res, next) => {
+    try {
+        const items = await Item.find().sort({ date: -1 });
+        res.json(items);
+    } catch (err) {
+        res.status(500).json({
+                success: false
+        });
+    }
+});
+
+// @route   POST /api/items
+// @desc    Create a new item.
+// @access  Public
+router.post('/', async (req, res, next) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+    
+    try {
+        await newItem.save()
+        res.json(newItem);
+    } catch(err) {
+       res.status(500).send({
+            message: 'internal server error'
+       });
+    }
+});
+
+// @route   DELETE /api/items/:id
+// @desc    Delete an item.
+// @access  Public
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const item = await Item.findById(req.params.id);
+        item.remove();
+        res.json({ success: true });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: 'internal server error'
+        });
+    }
+});
+
+module.exports = router;
