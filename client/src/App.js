4c266947964ffc6807189da721533eee122144ff
Molnár Antal Albert <krook1024@gmail.com>
2020-07-13 21:51:13 +0200

    Chose a darker theme for the application

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
 import './App.css';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
